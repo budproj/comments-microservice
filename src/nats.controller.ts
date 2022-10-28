96aef94539ad97c9d5aed38ebf482111486090fa
Rodrigo Antas <rodr.antas@gmail.com>
2022-10-28 16:18:18 -0300

    feat(comment-count): adding comment count to the routine messagepattern

--- src/nats.controller.ts
+++ src/nats.controller.ts
@@ -6,13 +6,16 @@ import {
   Payload,
   Transport,
 } from '@nestjs/microservices';
+import { Comment } from '@prisma/client';
 import { HealthCheckDBService } from './healthcheck.db.service';
+import { CommentService } from './services/comments.service';
 
 @Controller()
 export class NatsController {
   constructor(
     private healthCheckDB: HealthCheckDBService,
     @Inject('NATS_SERVICE') private client: ClientProxy,
+    private comments: CommentService,
   ) {}
 
   private readonly logger = new Logger(NatsController.name);
@@ -22,5 +25,19 @@ export class NatsController {
     const response = await this.healthCheckDB.patch(data.id);
 
     this.client.emit(data.reply, true);
+  }
+
+  @MessagePattern('comment-count', Transport.NATS)
+  async countComments(@Payload() entity: Comment['entity']) {
+    this.logger.log(
+      'New routine notification message to the comment count pattern with data:',
+      entity,
+    );
+
+    const commentCount = await this.comments.countComments({
+      where: { entity },
+    });
+
+    return commentCount;
   }
 }
