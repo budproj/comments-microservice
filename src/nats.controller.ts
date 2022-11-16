7048003f28632f225efe6aa99afccfc3a4b7de4d
Rodrigo Antas <rodr.antas@gmail.com>
2022-11-16 15:56:49 -0300

    feat(comment-routine): adding comment routine notification

---
+++ src/nats.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Inject, Logger } from '@nestjs/common';
+
+import {
+  ClientProxy,
+  MessagePattern,
+  Payload,
+  Transport,
+} from '@nestjs/microservices';
+import { Comment } from '@prisma/client';
+import { HealthCheckDBService } from './healthcheck.db.service';
+import { CommentService } from './services/comments.service';
+import { MessagingService } from './services/messaging.service';
+
+@Controller()
+export class NatsController {
+  constructor(
+    private healthCheckDB: HealthCheckDBService,
+    private messaging: MessagingService,
+    private comments: CommentService,
+  ) {}
+
+  private readonly logger = new Logger(NatsController.name);
+
+  @MessagePattern('health-check', Transport.NATS)
+  async onHealthCheck(@Payload() data: { id: string; reply: string }) {
+    const response = await this.healthCheckDB.patch(data.id);
+
+    this.messaging.emit(data.reply, true);
+  }
+
+  @MessagePattern('comment-count', Transport.NATS)
+  async countComments(@Payload() entity: Comment['entity']) {
+    this.logger.log(
+      'New routine notification message to the comment count pattern with data:',
+      entity,
+    );
+
+    const commentCount = await this.comments.countComments({
+      where: { entity },
+    });
+
+    return commentCount;
+  }
+}
