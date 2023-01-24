7a93642b5daa26403b64c95579bd712739d70523
Victor Perin <me@victorperin.ninja>
2023-01-24 13:59:53 -0300

    test(e2e): moving comments controller test to e2e test

---
+++ test/e2e/comments.test.ts
@@ -0,0 +1,90 @@
+import {
+  getPostgresConnectionString,
+  getRestConnectionString,
+} from './support-functions/generate-connection-strings';
+import { generateValidJwt } from './support-functions/generateJwt';
+import * as httpRequest from 'supertest';
+import { Comment, PrismaClient } from '@prisma/client';
+import { User } from 'src/types/User';
+import { listenAndReply } from './support-functions/nats-mock';
+import { randomUUID } from 'crypto';
+
+describe('REST Comments Controller', () => {
+  jest.setTimeout(120_000);
+  let url: string;
+  let validJwtToken: string;
+  let dbConnection: PrismaClient;
+
+  const userMock: User = {
+    id: '922ef72a-6c3c-4075-926a-3245cdeea75f',
+    companies: [{ id: '6164ec96-cd6f-4abc-ad25-9cca88cbb07c' }],
+    teams: [{ id: '968e8d90-c1dd-4d5c-948a-067e070ea269' }],
+  };
+
+  beforeAll(async () => {
+    url = getRestConnectionString(global.__api__);
+    validJwtToken = await generateValidJwt({ sub: '1234' });
+  });
+
+  beforeEach(async () => {
+    const postgresConStr = getPostgresConnectionString(global.__postgres__);
+
+    dbConnection = new PrismaClient({
+      datasources: { db: { url: postgresConStr } },
+    });
+
+    await dbConnection.$connect();
+  });
+
+  afterEach(async () => {
+    await dbConnection.comment.deleteMany({});
+    await dbConnection.$disconnect();
+  });
+
+  it('/comments (POST)', async () => {
+    // Arrange
+    listenAndReply('core-ports.verify-token', { sub: '1234' });
+    listenAndReply('core-ports.get-user-with-teams-by-sub', userMock);
+    listenAndReply('core-ports.get-user-companies', userMock.companies);
+
+    const entity = `objective:${randomUUID()}`;
+    const commentContent = 'Comentário do bom';
+
+    // Act
+    const result = await httpRequest(url)
+      .post(`/comments/${entity}`)
+      .set('Authorization', `Bearer ${validJwtToken}`)
+      .send({ content: commentContent });
+    const dbData = await dbConnection.comment.findMany();
+
+    // Assert
+    expect(result.statusCode).toBe(201);
+    expect(dbData).toHaveLength(1);
+    expect(dbData[0]).toHaveProperty('entity', entity);
+    expect(dbData[0]).toHaveProperty('content', commentContent);
+  });
+
+  it('/comments (GET)', async () => {
+    // Arrange
+    listenAndReply('core-ports.verify-token', { sub: '1234' });
+    listenAndReply('core-ports.get-user-with-teams-by-sub', userMock);
+    listenAndReply('core-ports.get-user-companies', userMock.companies);
+
+    const entity = `objective:${randomUUID()}`;
+    const comment: Comment = {
+      id: randomUUID(),
+      entity,
+      userId: randomUUID(),
+      content: 'just a normal comment',
+      createdAt: new Date(),
+    };
+
+    // Act
+    await dbConnection.comment.create({ data: comment });
+    const result = await httpRequest(url).get(`/comments/${entity}`);
+
+    // Assert
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toEqual(comment);
+  });
+});
